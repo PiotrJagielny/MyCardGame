cad6b4458d6aa8f2b9d13547ad334944ec80204c
PiotrJagielny <piotrek2002.09@gmail.com>
2023-03-23 16:08:43 +0100

    learning basic react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputField from './Components/InputField'
+
 
 function App() {
+
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      Hello world
+      <InputField todo={todo} setTodo={setTodo}/>
+
     </div>
   );
-}
+};
 
 export default App;
