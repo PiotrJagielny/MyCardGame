269fe1130cdf33b208354c0a1876175c06bbe53d
PiotrJagielny <piotrek2002.09@gmail.com>
2023-03-24 11:13:07 +0100

    trying to add drag and drop

--- frontend/src/Game_Related_Components/DeckBuildingComponents/DeckBuilderPage.tsx
+++ frontend/src/Game_Related_Components/DeckBuildingComponents/DeckBuilderPage.tsx
@@ -1,37 +1,63 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+import  './DeckBuilderPage.css';
 
-interface Card{
-    name: string;
+interface Card {
+  name: string;
 }
+
+const SomeData = [
+  { name: 'Card 1' },
+  { name: 'Card 2' },
+  { name: 'Card 3' },
+];
 
 const DeckBuilderPage = () => {
-    const [CardsData, setCardsData] = useState<Card[]>([]);
+  const [CardsData, setCardsData] = useState<Card[]>([]);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/DeckBuilder/GetAllCards')
-          .then(res => res.json())
-          .then((CardsData: Card[]) => {
-            setCardsData(CardsData);
-          })
-          .catch(console.error);
-        
-          console.log(CardsData);
-      }, []);
-      
-    
+  useEffect(() => {
+    fetch('http://localhost:8000/DeckBuilder/GetAllCards')
+      .then((res) => res.json())
+      .then((CardsData: Card[]) => {
+        setCardsData(CardsData);
+      })
+      .catch(console.error);
 
-    return (
-        <div>
-            DeckBuilderPage <br/>
+    console.log(CardsData);
+  }, []);
 
-            <p>All Cards:</p>
-            {CardsData.map(item => (
-                <div key={item.name}>
-                    <label>{item.name}</label>
-                </div>
-            ))}
-        </div>
-    )
-}
 
-export default DeckBuilderPage
+  
+
+  return (
+    <div>
+      DeckBuilderPage <br />
+
+      <p>All Cards:</p>
+
+      <DragDropContext onDragEnd={() => {}}>
+        <Droppable droppableId="name">
+          {(provided) => (
+            <div className="AllCards" ref={provided.innerRef} {...provided.droppableProps}>
+              <ul>
+                {SomeData.map((item, index) => (
+                  <Draggable key={item.name} draggableId={item.name} index={index}>
+                    {(provided) => (
+                      <li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} className="ggg">
+                        <div>{item.name} </div>
+                        <br />
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
+
+export default DeckBuilderPage;
