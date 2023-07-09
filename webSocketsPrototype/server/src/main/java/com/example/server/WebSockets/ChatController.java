640d772bbfaa81fb06fff3555fa3d00b3298726e
PiotrJagielny <piotrek2002.09@gmail.com>
2023-07-09 10:11:16 +0200

    prototype server completer

--- webSocketsPrototype/server/src/main/java/com/example/server/WebSockets/ChatController.java
+++ webSocketsPrototype/server/src/main/java/com/example/server/WebSockets/ChatController.java
@@ -7,6 +7,7 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +17,10 @@ public class ChatController {
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
     private static List<GameRoom> rooms = new ArrayList<>();
+    private static int maxGameId = 0;
 
     @MessageMapping("/findEnemy")
     public String receivePrivateMessage(@Payload String userName) {
-
-        System.out.println("szukanie!!!!!! dla: " + userName);
 
         if(rooms.isEmpty()) {
             rooms.add(new GameRoom(userName));
@@ -31,13 +31,27 @@ public class ChatController {
             }
             else {
                 rooms.get(rooms.size() - 1).setSecondPlayer(userName);
-                simpMessagingTemplate.convertAndSendToUser(userName, "/private", "Found enemy");
-                simpMessagingTemplate.convertAndSendToUser(rooms.get(rooms.size() - 1).getFirstPlayer(), "/private", "Found enemy");
+                maxGameId++;
+                simpMessagingTemplate.convertAndSendToUser(userName, "/private", "Found enemy:" + maxGameId);
+                try {
+                    //If message will be send one right after another, then two games will be created
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                simpMessagingTemplate.convertAndSendToUser(rooms.get(rooms.size() - 1).getFirstPlayer(), "/private", "Found enemy:" + maxGameId);
+
             }
         }
 
         return userName;
     }
+    @MessageMapping("/sendToEnemy")
+    public String sendMessageToEnemy(@Payload String userName) {
+        System.out.println("JEST WYSYLANIE DO ENEMY");
+        return userName;
+    }
+
     private boolean isGameRoomFull(GameRoom room) {
         return !room.getFirstPlayer().isEmpty() && !room.getSecondPlayer().isEmpty();
     }
