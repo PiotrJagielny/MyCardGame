4063a16ecbe27c5868cbb025f9d9b46251059b76
PiotrJagielny <piotrek2002.09@gmail.com>
2023-07-22 08:30:53 +0200

    changed server to local on development

--- frontend/src/Game_Unrelated_Components/EntryPageComponents/EntryPage.tsx
+++ frontend/src/Game_Unrelated_Components/EntryPageComponents/EntryPage.tsx
@@ -17,8 +17,8 @@ const EntryPage = () => {
 
 
 
-    // dispatch({type: "SET_SERVER_URL", payload: 'http://localhost:8000'});
-    dispatch({type: "SET_SERVER_URL", payload: 'https://card-game-9dmt.onrender.com'});
+    dispatch({type: "SET_SERVER_URL", payload: 'http://localhost:8000'});
+    // dispatch({type: "SET_SERVER_URL", payload: 'https://card-game-9dmt.onrender.com'});
     navigate("/Main");
   }
 
@@ -32,7 +32,7 @@ const EntryPage = () => {
         </h1>
         <h6>You can build deck, as well as play duel with yourself. If you click find enemy two times you will start game with yourself.
           I made it so that you can test playing duel because there arent many people that play my game. As this is hosted for free, all api calls are very slow. To enter deck 
-          builder there is some waiting, you have to click button a few times. To play game with yourself at duel page, you have to sometime do refreshing and loading data.
+          builder there is some waiting. To play game with yourself at duel page, you have to sometime do refreshing and loading data.
         </h6>
 
       </div>
