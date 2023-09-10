28b0acbc4b2a7781a0a4d4292168e91c7f682b46
PiotrJagielny <piotrek2002.09@gmail.com>
2023-09-10 14:09:09 +0200

    added new icon, new title

--- frontend/src/Game_Related_Components/DuelComponents/DuelPage.tsx
+++ frontend/src/Game_Related_Components/DuelComponents/DuelPage.tsx
@@ -206,9 +206,8 @@ const DuelPage = () => {
 
 
 
-
   const fetchData = <T,>(url: string,data: T ,setter: React.Dispatch<React.SetStateAction<T>>) => {
-    fetch(url)
+    return fetch(url)
       .then((res) => res.json())
       .then((data: T) => {
         setter(data);
@@ -216,32 +215,32 @@ const DuelPage = () => {
       .catch(console.error);
   }
 
-
-  
   const fetchCardsData = () => {
     fetch(`${serverURL}/Duel/getEnemyOf/${userName}/${gameID}`)
       .then((res) => res.text())
       .then((userEnemy: string) => {
         setEnemyName(userEnemy);
-        fetchData<Card[]>(`${serverURL}/Duel/getHandCards/${userName}/${gameID}`, cardsInHand ,setCardsInHand);
-        fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userName}/${firstRow}/${gameID}`,cardsOnBoard ,setCardsOnBoard);
-        fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userName}/${secondRow}/${gameID}`, cardsOnSecondRow ,setCardsOnSecondRow);
-        fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userName}/${thirdRow}/${gameID}`, cardsOnThirdRow ,setCardsOnThirdRow);
-        fetchData<boolean>(`${serverURL}/Duel/isTurnOf/${userName}/${gameID}`, isTurnOfPlayer1 ,setIsTurnOfPlayer1);
-        fetchData<number>(`${serverURL}/Duel/getWonRounds/${userName}/${gameID}`, wonRounds ,setWonRounds);
-        fetchData<boolean>(`${serverURL}/Duel/didWon/${userName}/${gameID}`, didWon ,setDidWon);
-        fetchData<number[]>(`${serverURL}/Duel/getRowsPoints/${userName}/${gameID}`, pointsOnRows,setPointsOnRows);
-        fetchData<string[]>(`${serverURL}/Duel/getRowsStatus/${userName}/${gameID}`, rowsStatus,setRowsStatus);
+        Promise.all([
+          fetchData<Card[]>(`${serverURL}/Duel/getHandCards/${userName}/${gameID}`, cardsInHand ,setCardsInHand),
+          fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userName}/${firstRow}/${gameID}`,cardsOnBoard ,setCardsOnBoard),
+          fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userName}/${secondRow}/${gameID}`, cardsOnSecondRow ,setCardsOnSecondRow),
+          fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userName}/${thirdRow}/${gameID}`, cardsOnThirdRow ,setCardsOnThirdRow),
+          fetchData<boolean>(`${serverURL}/Duel/isTurnOf/${userName}/${gameID}`, isTurnOfPlayer1 ,setIsTurnOfPlayer1),
+          fetchData<number>(`${serverURL}/Duel/getWonRounds/${userName}/${gameID}`, wonRounds ,setWonRounds),
+          fetchData<boolean>(`${serverURL}/Duel/didWon/${userName}/${gameID}`, didWon ,setDidWon),
+          fetchData<number[]>(`${serverURL}/Duel/getRowsPoints/${userName}/${gameID}`, pointsOnRows,setPointsOnRows),
+          fetchData<string[]>(`${serverURL}/Duel/getRowsStatus/${userName}/${gameID}`, rowsStatus,setRowsStatus),
 
-        fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userEnemy}/${firstRow}/${gameID}`,enemyCardsOnFirstRow ,setenemyCardsOnFirstRow);
-        fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userEnemy}/${secondRow}/${gameID}`, enemyCardsOnSecondRow,setenemyCardsOnSecondRow);
-        fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userEnemy}/${thirdRow}/${gameID}`, enemyCardsOnThirdRow ,setCardsOnThirdRow2);
-        fetchData<boolean>(`${serverURL}/Duel/isTurnOf/${userEnemy}/${gameID}`, isEnemyTurn ,setisEnemyTurn);
-        fetchData<number>(`${serverURL}/Duel/getWonRounds/${userEnemy}/${gameID}`, enemyWonRounds ,setenemyWonRounds);
-        fetchData<boolean>(`${serverURL}/Duel/didWon/${userEnemy}/${gameID}`, didEnemyWon ,setdidEnemyWon);
-        fetchData<number[]>(`${serverURL}/Duel/getRowsPoints/${userEnemy}/${gameID}`, enemyPointsOnRows,setEnemyPointsOnRows);
-        fetchData<string[]>(`${serverURL}/Duel/getRowsStatus/${userEnemy}/${gameID}`, rowsStatus,setRowsStatus);
-        fetchData<number>(`${serverURL}/Duel/getHandSize/${userEnemy}/${gameID}`, enemyHandSize,setEnemyHandSize);
+          fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userEnemy}/${firstRow}/${gameID}`,enemyCardsOnFirstRow ,setenemyCardsOnFirstRow),
+          fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userEnemy}/${secondRow}/${gameID}`, enemyCardsOnSecondRow,setenemyCardsOnSecondRow),
+          fetchData<Card[]>(`${serverURL}/Duel/getCardsOnRow/${userEnemy}/${thirdRow}/${gameID}`, enemyCardsOnThirdRow ,setCardsOnThirdRow2),
+          fetchData<boolean>(`${serverURL}/Duel/isTurnOf/${userEnemy}/${gameID}`, isEnemyTurn ,setisEnemyTurn),
+          fetchData<number>(`${serverURL}/Duel/getWonRounds/${userEnemy}/${gameID}`, enemyWonRounds ,setenemyWonRounds),
+          fetchData<boolean>(`${serverURL}/Duel/didWon/${userEnemy}/${gameID}`, didEnemyWon ,setdidEnemyWon),
+          fetchData<number[]>(`${serverURL}/Duel/getRowsPoints/${userEnemy}/${gameID}`, enemyPointsOnRows,setEnemyPointsOnRows),
+          fetchData<string[]>(`${serverURL}/Duel/getRowsStatus/${userEnemy}/${gameID}`, rowsStatus,setRowsStatus),
+          fetchData<number>(`${serverURL}/Duel/getHandSize/${userEnemy}/${gameID}`, enemyHandSize,setEnemyHandSize)
+        ]).then((values) => { console.log("all fetched")})
       }).catch(console.error);
   }
 
@@ -328,7 +327,9 @@ const DuelPage = () => {
     else if(destination.droppableId === thirdRowId){
       setCardPlayedRow(2);
     }
-    if(Number(result.draggableId) === playChainCard.id && playChainCard.name !== "") {
+
+
+    if(Number(result.draggableId) === playChainCard.id) {
       setCardDragged(playChainCard);
     }
     else {
